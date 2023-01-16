85f918267d4bf8e53195454a8d0c9ec877b3141a
threen4846 <threen4846@naver.com>
2023-01-16 16:18:51 +0900

    change Key

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,6 @@
 const weather = document.querySelector("#weather span:first-child");
 const city = document.querySelector("#weather span:last-child");
-const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
+const API_KEY = "155a7440f673452b160255ce8dc11d24";
 
 function onGeoOk(position) {
   const lat = position.coords.latitude;
