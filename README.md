e560b09c1cbc0c2c9b1583ecaaed5842231690ab
threen4846 <threen4846@naver.com>
2023-01-16 15:38:56 +0900

    make app

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# CromeAppWithVanillaJS
-바닐라 JS로 크롬 앱 만들기입니다.
+# Javascript For Beginners
+
+Learning Javascript by Making a Productivity App
--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,10 @@
 const weather = document.querySelector("#weather span:first-child");
 const city = document.querySelector("#weather span:last-child");
-const API_KEY = "155a7440f673452b160255ce8dc11d24";
+const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
 
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  console.log(lat, lon);
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
